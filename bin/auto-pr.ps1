0c795e6b7c42e28242da2c23a5a47254c3aa0fdd
Jeff Lanzarotta <jlanzarotta@matw.com>
2024-09-12 13:04:56 -0400

    Changed main to master.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "jlanzarotta/scoop-khronos:main"
+    [String]$upstream = "jlanzarotta/scoop-khronos:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
